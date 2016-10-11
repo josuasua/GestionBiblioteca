2c4a0871f84b476b1a402383e40a89d220b4ffdd
josuasua <josuasua@hotmail.com>
2016-10-11 14:35:56 +0200

    añadiendo carpetas, DAL y capa persistencia usuario. usango NuGet para importar librerias de jQuery

---
+++ GestionBiblioteca/DAL/UsuarioRepositoryImp.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Data;
+using System.Configuration;
+
+
+namespace WebApplication1.DAL
+{
+    public class UsuarioRepositoryImp : UsuarioRepository
+    {
+        private string conexionString = ConfigurationManager.ConnectionStrings["GESTLIBRERIAConnectionString"].ConnectionString;
+        public Guid create(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(Guid codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Usuario> getAll()
+        {
+            List<Usuario> usuarios = null;
+            const string SQL = "mostrarUsuarios";
+
+            using (SqlConnection conexion = new SqlConnection(conexionString))
+            {
+
+                SqlCommand command = new SqlCommand(SQL, conexion);
+                command.CommandText = SQL;
+                command.CommandType = CommandType.StoredProcedure;
+                conexion.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) //si devuelve datos, hasRows es un atributo del objeto
+                    {
+                        usuarios = new List<Usuario>();
+                        Usuario usuario = null;
+                        while (reader.Read()) //cada vuelta que da sacando datos
+                        {
+                            usuario = parseUsuario(reader);
+                            usuarios.Add(usuario);
+                        }
+                    }
+                }
+            }
+
+            return usuarios;
+
+        }
+
+        public Usuario update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario getById(Guid codigo)
+        {
+            Usuario usuario = null;
+            const string SQL = "mostrarUnUsuario";
+            using (SqlConnection conexion = new SqlConnection(conexionString))
+            {
+
+                SqlCommand command =  new SqlCommand (SQL, conexion);
+                command.CommandText = SQL;
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@idUsuario", codigo);
+                //command.Connection = conexion;
+                conexion.Open();
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) //si devuelve datos
+                    {
+
+                        while (reader.Read()) //cada vuelta que da sacando datos
+                        {
+                            usuario = parseUsuario(reader);
+                        }
+                    }
+                }
+            }
+            return usuario;
+        }
+
+        private Usuario parseUsuario(SqlDataReader reader)
+        {
+            Usuario usuario = new Usuario();
+            usuario.CodigoUsuario = new Guid(reader["idUsuario"].ToString());
+            usuario.Nombre = reader["nombre"].ToString();
+            usuario.Apellidos = reader["apellidos"].ToString();
+            usuario.Email = reader["email"].ToString();
+            usuario.Password = reader["password"].ToString();
+            usuario.User = reader["usuario"].ToString();
+            usuario.FNacimiento = Convert.ToDateTime(reader["fnacimiento"].ToString());
+
+
+            return usuario;
+        }
+    }
+}
---
+++ GestionBiblioteca/Models/Usuario.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Usuario
+    {
+        private string _nombre;
+        private string _apellidos;
+        private string _password;
+        private string _user;
+        private string _email;
+        private string _dni;
+        private DateTime _fNacimiento;
+        private Guid _codigoUsuario;
+        public Usuario()
+        {
+            this._nombre = "";
+            this._apellidos = "";
+            this._password = "";
+            this._user = "";
+            this._email = "";
+            this._dni = "";
+            this._fNacimiento = new DateTime();
+            this._codigoUsuario = new Guid("-1");
+            
+        }
+        public Usuario(string _nombre, string _dni, string _apellidos, string _password, string _user, string _email, DateTime _fNacimiento, Guid _codigoUsuario)
+        {
+           
+            this._nombre = _nombre;
+            this._apellidos = _apellidos;
+            this._password = _password;
+            this._user = _user;
+            this._email = _email;
+            this._dni = _dni;
+            this._fNacimiento = _fNacimiento;
+            this._codigoUsuario = _codigoUsuario;
+        }
+
+        public Guid CodigoUsuario
+        {
+            get
+            {
+                return _codigoUsuario;
+            }
+
+            set
+            {
+                _codigoUsuario = value;
+            }
+        }
+
+        public DateTime FNacimiento
+        {
+            get
+            {
+                return _fNacimiento;
+            }
+
+            set
+            {
+                _fNacimiento = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+
+            set
+            {
+                _user = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public string Apellidos
+        {
+            get
+            {
+                return _apellidos;
+            }
+
+            set
+            {
+                _apellidos = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public string Dni
+        {
+            get
+            {
+                return _dni;
+            }
+
+            set
+            {
+                _dni = value;
+            }
+        }
+    }
+}
